46c026a37d466070ff8a9fc35cbeda40a78be9a9
yeyMelexis <yeremenkoyevgeniy@ukr.net>
2016-12-14 21:00:40 +0200

    lab1

---
+++ task_7.v
@@ -0,0 +1,33 @@
+module task_7(clk, 
+              n_rst, 
+              Left_in, 
+              Right_in, 
+              data_in, 
+              data_out);
+
+parameter DATA_WIDTH = 4;
+              
+input clk, n_rst, Right_in, Left_in;
+input   [DATA_WIDTH-1:0]  data_in;
+output  [DATA_WIDTH-1:0]  data_out; 
+reg     [DATA_WIDTH-1:0]  data_out;
+
+always @(posedge clk or negedge n_rst) begin
+  if(!n_rst) begin
+    data_out <= 0;
+  end
+  else begin
+    case({Right_in, Left_in})
+      2'b00: 
+        data_out <= data_in;
+      2'b01:
+        data_out <= {data_out[DATA_WIDTH-2:0], Left_in};
+      2'b10:
+        data_out <= {Right_in, data_out[DATA_WIDTH-1:1]};       
+    endcase
+  end
+  
+  
+end
+              
+endmodule
---
+++ task_7_tb.v
@@ -0,0 +1,45 @@
+module task_7_tb();
+  
+parameter DATA_WIDTH = 4;
+              
+reg clk, n_rst, Right_in, Left_in;
+reg   [DATA_WIDTH-1:0]  data_in;
+wire  [DATA_WIDTH-1:0]  data_out; 
+  
+  initial begin
+    clk = 0;
+    forever #1 clk = ~clk;
+  end
+  
+  initial begin
+    Right_in  = 0;
+    Left_in   = 0;
+    data_in   = 0;
+    
+       n_rst = 1'b1;
+    #1 n_rst = 1'b0;
+    #2 n_rst = 1'b1;
+    
+    repeat(10) begin 
+      data_in = $random();
+      Right_in = $random();
+      Left_in = $random();
+       @(posedge clk);
+    end
+    
+    $finish;
+  end
+  
+  
+task_7 task_71(.clk(clk), 
+              .n_rst(n_rst), 
+              .Left_in(Left_in), 
+              .Right_in(Right_in), 
+              .data_in(data_in), 
+              .data_out(data_out)
+              );
+
+
+  
+  
+endmodule
